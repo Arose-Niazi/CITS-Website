dc84cfd4649fa84ed9f1c4e16dad7e2bfe32f790
Arose Niazi <arose.niazi@yahoo.com>
2020-03-26 18:20:51 +0500

    File Added

---
+++ testing.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<head>
+  <?php
+    include('header.php');
+  ?>
+  <link rel="stylesheet" href="CSS/Main.css">
+  <link rel="stylesheet" href="CSS/Theme.css">
+  <link rel="stylesheet" href="CSS/slick.css">
+  <script src="JavaScript/Main.js"></script>
+  <script src="JavaScript/testing.js"></script>
+</head>
+<body style="background-color: #F4F4F4;">
+<div name="top" class="slider" id="slider1_container">
+        
+  <div u="image" class="polaroid">
+      <img src="Images/events/treasureHunt.jpg" alt="5 Terre" style="width:100%">
+      <div class="container des">
+          <p>
+              <span class="heading">Treasure Hunt..</span><br><span
+                  class="description">Find clues, and win 10k Pkr!.</span>
+          </p>
+          <div class="text-right"><button class="allBtns"
+                  onclick="top.location='treasureHunt.html'"><span>Read
+                      More</span><img src="Images/arrowRY.png" width="20"
+                      height="20"></button>
+          </div>
+      </div>
+  </div>
+  <div u="image" class="polaroid">
+      <img src="Images/events/fifa.jpg" alt="5 Terre" style="width:100%">
+      <div class="container des">
+          <p><span class="heading">FIFA 2020...</span><br><span class="description">
+              Come and show us what you've got!</span></p>
+          <div class="text-right"><button class="allBtns"
+                  onclick="top.location='fifa.html'"><span>Read
+                      More</span><img src="Images/arrowRY.png" width="20"
+                      height="20"></button>
+                  </div>
+      </div>
+  </div>
+  <div u="image" class="polaroid">
+      <img src="Images/events/futsal.jpeg" alt="5 Terre" style="width:100%">
+      <div class="container des">
+        <p><span class="heading">Futsal...</span><br><span class="description">
+            Come play with us!</span></p>
+          <div class="text-right"><button class="allBtns"
+                  onclick="top.location='futsal.html'"><span>Read
+                      More</span><img src="Images/arrowRY.png" width="20"
+                      height="20"></button></div>
+      </div>
+  </div>
+  <div u="image" class="polaroid">
+      <img src="Images/events/cod.jpeg" alt="5 Terre" style="width:100%">
+      <div class="container des">
+        <p><span class="heading">COD4...</span><br><span class="description">
+            Kill your enemies!</span></p>
+                <div class="text-right"><button class="allBtns"
+                  onclick="top.location='futsal.html'"><span>Read
+                      More</span><img src="Images/arrowRY.png" width="20"
+                      height="20"></button></div>
+      </div>
+  </div>
+  <div u="image" class="polaroid">
+      <img src="Images/events/party.jpg" alt="5 Terre" style="width:100%">
+      <div class="container des">
+        <p><span class="heading">Wellcome Party...</span><br><span class="description">
+            Live in present and find a reason to celebrate</span></p>
+          <div class="text-right"><button class="allBtns"
+                  onclick="top.location='party.html'"><span>Read
+                      More</span><img src="Images/arrowRY.png" width="20"
+                      height="20"></button></div>
+      </div>
+  </div>
+
+</div>
+
+</body>
