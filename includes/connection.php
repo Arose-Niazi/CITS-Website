319b495e55220ed37a7703025c29f4fdf6c874d1
Arose Niazi <arose.niazi@yahoo.com>
2021-02-16 13:38:40 +0500

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,10 +1,10 @@
 <?php
 	/* Database credentials. Assuming you are running MySQL
 	server with default setting (user 'root' with no password) */
-	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'CITS');
+	define('DB_SERVER', '51.210.180.137');
+	define('DB_USERNAME', 'cits');
+	define('DB_PASSWORD', 'VowdBqPtzh5i88LK');
+	define('DB_NAME', 'cits');
 	
 	/* Attempt to connect to MySQL database */
 	$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
