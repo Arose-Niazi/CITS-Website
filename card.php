68fa34fa847bd561dbb2a88db4c962bdf1a9bc5f
123qwe456 <aeman098.fatima@gmail.com>
2021-02-26 13:04:21 +0500

    Card page

---
+++ card.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+	<?php
+        include('includes/header.php');
+        require_once('includes/connection.php');
+	?>
+
+    <link rel="stylesheet" href="CSS/profile.css">
+    <link href="CSS/card.css">
+
+<div class="cardBox">
+    <div class="personImg">
+        <img class="person" src="person.png">
+    </div>
+    <div class="text">
+        <span class="bold larger">Chacha <span class="blue">Bultoro</span></span></br>
+        <span><span class="blue">Member</span></span></br></br>
+        <span class="bold largest">FA18-BSE-036</span><br><br>
+        <span class="large bold">JOINED</span></br>
+        <span class="large bold">FALL 20</span>
+    </div>
+</div>
